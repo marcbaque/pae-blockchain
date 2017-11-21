84bea1570114d848488e23a7ed90ec2fb3c64b7f
smalagon96 <smalagon96@gmail.com>
2017-11-21 09:45:10 +0100

    Modified test with async/await

--- test/test.js
+++ test/test.js
@@ -1,23 +1,43 @@
-var UserFactory = artifacts.require("./UserFactory.sol");
-var EventFactory = artifacts.require("./EventFactory.sol");
-var Artist = artifacts.require("./Artist.sol");
-var Organization = artifacts.require("./Organization.sol");
-//var User = artifacts.require("./User.sol");
-var EventContract = artifacts.require("./EventContract.sol");
+let UserFactory = artifacts.require("./UserFactory.sol");
+let EventFactory = artifacts.require("./EventFactory.sol");
+let Artist = artifacts.require("./Artist.sol");
+let Organization = artifacts.require("./Organization.sol");
+let EventContract = artifacts.require("./EventContract.sol");
 
-var uf;
-var ef;
-var artists = [];
+let uf;
+let ef;
+let artists = [];
 
-var art1;
-var createdEvent;
+let art1;
+let createdEvent;
 
-var testLog;
+let testLog;
 
 contract("Test", function(accounts) {
-    it("should create User&Event Factories and linking them to each other", function() {
+
+    it("should create User&Event Factories and linking them to each other", async function() {
+
         testLog ="------------------------------------------------------------------------------------\n\n";
         testLog += "Creation of User&Event Factories and link them to each other\n\n";
+
+        uf = await UserFactory.new({from: accounts[0]});
+        testLog += "   UserFactory address: " + uf.address + "\n";
+
+        ef = await EventFactory.new({from: accounts[0]});
+        testLog += "   EventFactory address: " + ef.address + "\n\n";
+
+        await ef.initializeUserFactory(uf.address,{from: accounts[0]});
+        await uf.initializeEventFactory(ef.address,{from: accounts[0]});
+        
+        let ufStoredInEf = await ef.userFactory.call();
+        testLog += "   UserFactory as stored in EventFactory: " + ufStoredInEf + "\n";
+        assert(uf.address== ufStoredInEf, "UF in EF not valid");
+        
+        let efStoredInUf = await uf.eventFactory.call();
+        testLog += "   EventFactory as stored in UserFactory: " + efStoredInUf + "\n\n";
+        assert(ef.address== efStoredInUf, "EF in UF not valid");
+
+    /*     OLD SCHOOL WAY:   
         return UserFactory.new({from: accounts[0]}).then(function(_uf) {
             uf = _uf;
             testLog += "   UserFactory address: " + uf.address + "\n";
@@ -37,10 +57,13 @@ contract("Test", function(accounts) {
         }).then(function(efStoredInUf) {
             testLog += "   EventFactory as stored in UserFactory: " + efStoredInUf + "\n\n";
             assert(ef.address== efStoredInUf, "EF in UF not valid");
-        });
-    });
+        }); */
+    
+    }); 
 
-    it("should create two artists", function() {
+
+    it("should create two artists", async function() {
+
         testLog += "------------------------------------------------------------------------------------\n\n";
         testLog += "Creation of artists\n\n";
         var artistCreated = uf.artistCreated();
@@ -54,15 +77,27 @@ contract("Test", function(accounts) {
             //testLog += "Artists array:  " + artists + "\n";
             
         });
-        return uf.createArtist("Furi Helium", "666", "furihelium@gmail.com", {from: accounts[1]})
-        .then(function(createdArtist) {
-            //artist1 = createdArtist;
-            setTimeout( () => artistCreated.stopWatching(), 2000);
-            return uf.createArtist("Elias' band", "777", "eliasmola@gmail.com", {from: accounts[2]});
-        });
+
+        let createdArtist = await uf.createArtist("Furi Helium", "666", "furihelium@gmail.com", {from: accounts[1]});
+        setTimeout( () => artistCreated.stopWatching(), 2000);
+        await uf.createArtist("Elias' band", "777", "eliasmola@gmail.com", {from: accounts[2]});
     });
 
-    it("artist1 should be able to create an event", function() {
+    it("artist1 should be able to create an event", async function() {
+
+        art1 = await Artist.at(artists[1]);
+        await art1.createEvent("Sabis Fest", "Description");
+        createdEvent = await art1.events.call(0);
+        setTimeout( () => {
+            testLog += "------------------------------------------------------------------------------------\n\n";
+            testLog += "Creation of an event\n\n"; 
+            testLog += "    Address of the event: " + createdEvent + "\n\n";
+            testLog += "------------------------------------------------------------------------------------\n\n"; 
+        }, 1000);
+        setTimeout( () => console.log(testLog), 1500);
+        assert(createdEvent != null, "events del artista es nulo");
+
+    /* OLD SCHOOL WAY:
         return Artist.at(artists[1]).
         then(function(_art1) {
             art1 = _art1;
@@ -80,11 +115,12 @@ contract("Test", function(accounts) {
             }, 1000);
             setTimeout( () => console.log(testLog), 1500);
             assert(createdEvent != null, "events del artista es nulo");
-        })
+        }) */
         
     });
 
-    /* it("artist1 should be able to add artist2 as an organizer of the event", function() {
+    /* 
+        it("artist1 should be able to add artist2 as an organizer of the event", function() {
 
         return Artist.at(artists[2]).
         then(function(_art2) {
@@ -95,6 +131,6 @@ contract("Test", function(accounts) {
         }).then(function(_eventOrganizer1) {
             eventOrganizer1 = _eventOrganizer1;
             testLog += "Event organizer 2: " + eventOrganizer2);
-        })
-    }); */
+        }) */
+     
 });
