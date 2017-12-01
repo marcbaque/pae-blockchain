27928799a879512241f21bee8f9ca4f89205ae36
jordiestape <estape.jordi@gmail.com>
2017-12-01 10:21:11 +0100

    prepare BSToken for deployment

---
+++ contracts/bs-token/Auth.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.2;
+
+import "PermissionManager.sol";
+
+contract Auth {
+    PermissionManager pm;
+    address public merchant;
+
+    modifier onlyAdminOrMerchant {
+        if (!pm.getNetworkAdmin(pm.getRol(msg.sender)) && msg.sender != merchant) throw;
+        _;
+    }
+
+    modifier onlyAdmin {
+        if (!pm.getNetworkAdmin(pm.getRol(msg.sender))) throw;
+        _;
+    }
+
+    function init(address theMerchant, address permissionManagerAddress) internal {
+        merchant = theMerchant;
+        pm = PermissionManager(permissionManagerAddress);
+    }
+}
